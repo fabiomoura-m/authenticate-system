fc67612446f85531d290118c06c5316fe584ad7e
Fabio de Moura Martins <fabio.mmouras@hotmail.com>
2024-06-27 16:55:54 -0300

    feat: add página home

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,31 @@
+import { LoginButton } from '@/components/auth/login-button';
+import { Button } from '@/components/ui/button';
+import { cn } from '@/lib/utils';
+import { Poppins } from 'next/font/google';
+
+const poppins = Poppins({ subsets: ['latin'], weight: ['600'] });
+
 export default function Home() {
-  return (
-    <p>Hello</p>
-  );
+    return (
+        <main className="flex h-full flex-col items-center justify-center bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-sky-400 to-blue-800">
+            <div className="space-y-6 text-center">
+                <h1
+                    className={cn(
+                        'text-6xl font-semibold text-white drop-shadow-md',
+                        poppins.className
+                    )}
+                >
+                    🔐 Auth
+                </h1>
+                <p className="text-white text-lg">Serviço de autenticação</p>
+                <div>
+                    <LoginButton>
+                        <Button variant="secondary" size="lg">
+                            Sign in
+                        </Button>
+                    </LoginButton>
+                </div>
+            </div>
+        </main>
+    );
 }
