30861b5b6f16d7d63920f41cf350a5dd67f73020
Fabio de Moura Martins <fabio.mmouras@hotmail.com>
2024-07-01 09:57:15 -0300

    feat: add página de registro

---
+++ components/auth/register-form.tsx
@@ -0,0 +1,125 @@
+'use client';
+
+import CardWrapper from '@/components/auth/card-wrapper';
+import { RegisterSchema } from '@/schemas';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useForm } from 'react-hook-form';
+import { z } from 'zod';
+import {
+    Form,
+    FormControl,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage
+} from '@/components/ui/form';
+import { Input } from '@/components/ui/input';
+import { Button } from '@/components/ui/button';
+import FormError from '@/components/form-error';
+import FormSuccess from '@/components/form-success';
+import { login } from '@/actions/login';
+import { useState, useTransition } from 'react';
+
+const RegisterForm = () => {
+    const [error, setError] = useState<string | undefined>('');
+    const [success, setSuccess] = useState<string | undefined>('');
+    const [isPending, startTransition] = useTransition();
+
+    const form = useForm<z.infer<typeof RegisterSchema>>({
+        resolver: zodResolver(RegisterSchema),
+        defaultValues: {
+            email: '',
+            password: '',
+            name: ''
+        }
+    });
+
+    const onSubmit = (values: z.infer<typeof RegisterSchema>) => {
+        setError('');
+        setSuccess('');
+
+        console.log(values);
+    };
+
+    return (
+        <CardWrapper
+            headerLabel="Criar uma conta"
+            backButtonLabel="Já possui uma conta?"
+            backButtonHref="/auth/login"
+            showSocial
+        >
+            <Form {...form}>
+                <form
+                    onSubmit={form.handleSubmit(onSubmit)}
+                    className="space-y-6"
+                >
+                    <div className="space-y-4">
+                        <FormField
+                            control={form.control}
+                            name="name"
+                            render={({ field }) => (
+                                <FormItem>
+                                    <FormLabel>Nome</FormLabel>
+                                    <FormControl>
+                                        <Input
+                                            {...field}
+                                            disabled={isPending}
+                                            placeholder="Seu nome"
+                                        />
+                                    </FormControl>
+                                    <FormMessage />
+                                </FormItem>
+                            )}
+                        />
+                        <FormField
+                            control={form.control}
+                            name="email"
+                            render={({ field }) => (
+                                <FormItem>
+                                    <FormLabel>E-mail</FormLabel>
+                                    <FormControl>
+                                        <Input
+                                            {...field}
+                                            disabled={isPending}
+                                            placeholder="fabio@exemplo.com"
+                                        />
+                                    </FormControl>
+                                    <FormMessage />
+                                </FormItem>
+                            )}
+                        />
+                        <FormField
+                            control={form.control}
+                            name="password"
+                            render={({ field }) => (
+                                <FormItem>
+                                    <FormLabel>Senha</FormLabel>
+                                    <FormControl>
+                                        <Input
+                                            {...field}
+                                            type="password"
+                                            placeholder="******"
+                                            disabled={isPending}
+                                        />
+                                    </FormControl>
+                                    <FormMessage />
+                                </FormItem>
+                            )}
+                        />
+                    </div>
+                    <FormError message={error} />
+                    <FormSuccess message={success} />
+                    <Button
+                        type="submit"
+                        className="w-full"
+                        disabled={isPending}
+                    >
+                        Criar sua conta
+                    </Button>
+                </form>
+            </Form>
+        </CardWrapper>
+    );
+};
+
+export default RegisterForm;
--- schemas/index.ts
+++ schemas/index.ts
@@ -8,3 +8,15 @@ export const LoginSchema = z.object({
         message: 'É necessário uma senha'
     })
 });
+
+export const RegisterSchema = z.object({
+    email: z.string().email({
+        message: 'E-mail incompleto'
+    }),
+    password: z.string().min(6, {
+        message: 'Mínimo de 6 caracteres'
+    }),
+    name: z.string().min(3, {
+        message: 'Nome incompleto'
+    })
+});
