0f894557d41d355be372edd0efa4585c55f7bccd
Fabio de Moura Martins <fabio.mmouras@hotmail.com>
2024-07-01 14:01:24 -0300

    feat: implementa o registro de um novo usuário ao banco de dados

--- actions/register.ts
+++ actions/register.ts
@@ -1,7 +1,11 @@
 'use server';
 
-import { RegisterSchema } from '@/schemas';
+import * as bcrypt from 'bcrypt';
 import { z } from 'zod';
+
+import { getUserByEmail } from '@/data/user';
+import { db } from '@/lib/db';
+import { RegisterSchema } from '@/schemas';
 
 export const register = async (values: z.infer<typeof RegisterSchema>) => {
     const validatedFields = RegisterSchema.safeParse(values);
@@ -10,5 +14,27 @@ export const register = async (values: z.infer<typeof RegisterSchema>) => {
         return { error: 'Campos inválidos!' };
     }
 
-    return { success: 'Email enviado!' };
+    const { email, password, name } = validatedFields.data;
+
+    const saltRounds = 10;
+    const salt = await bcrypt.genSalt(saltRounds);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const existingUser = await getUserByEmail(email);
+
+    if (existingUser) {
+        return { error: 'Email já cadastrado!' };
+    }
+
+    await db.user.create({
+        data: {
+            name,
+            email,
+            password: hashedPassword
+        }
+    });
+
+    // TODO: Send verification token email
+
+    return { success: 'Usuário criado!' };
 };
