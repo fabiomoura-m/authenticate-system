537b116ba58c13de5349cf9bae1ea5df95e2580e
Fabio de Moura Martins <fabio.mmouras@hotmail.com>
2024-07-02 08:53:58 -0300

    feat: configura a autenticação por login e senha com redirecionamento

--- actions/login.ts
+++ actions/login.ts
@@ -1,6 +1,9 @@
 'use server';
 
+import { signIn } from '@/auth';
+import { DEFAULT_LOGIN_REDIRECT } from '@/routes';
 import { LoginSchema } from '@/schemas';
+import { AuthError } from 'next-auth';
 import { z } from 'zod';
 
 export const login = async (values: z.infer<typeof LoginSchema>) => {
@@ -10,5 +13,24 @@ export const login = async (values: z.infer<typeof LoginSchema>) => {
         return { error: 'Campos inválidos!' };
     }
 
-    return { success: 'Email enviado!' };
+    const { email, password } = validatedFields.data;
+
+    try {
+        await signIn('credentials', {
+            redirectTo: DEFAULT_LOGIN_REDIRECT,
+            email,
+            password
+        });
+    } catch (error) {
+        if (error instanceof AuthError) {
+            switch (error.type) {
+                case 'CallbackRouteError':
+                    return { error: 'Email ou senha não conferem!' };
+                default:
+                    return { error: 'Algo deu errado!' };
+            }
+        }
+
+        throw error;
+    }
 };
--- auth.config.ts
+++ auth.config.ts
@@ -1,4 +1,36 @@
-import GitHub from "next-auth/providers/github"
-import type { NextAuthConfig } from "next-auth"
- 
-export default { providers: [GitHub] } satisfies NextAuthConfig
+import bcrypt from 'bcryptjs';
+import type { NextAuthConfig } from 'next-auth';
+import Credentials from 'next-auth/providers/credentials';
+
+import { LoginSchema } from '@/schemas';
+import { getUserByEmail } from '@/data/user';
+
+export default {
+    providers: [
+        Credentials({
+            credentials: {
+                email: {},
+                password: {}
+            },
+            async authorize(credentials) {
+                const validateFields = LoginSchema.safeParse(credentials);
+
+                if (validateFields.success) {
+                    const { email, password } = validateFields.data;
+
+                    const user = await getUserByEmail(email);
+                    if (!user || !user.password) return null;
+
+                    const passwordsMath = await bcrypt.compare(
+                        password,
+                        user.password
+                    );
+
+                    if (passwordsMath) return user;
+                }
+
+                return null;
+            }
+        })
+    ]
+} satisfies NextAuthConfig;
