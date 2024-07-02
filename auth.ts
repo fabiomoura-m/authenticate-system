2800a7e01fab45fd3051b092a94d4f9fae3c9b42
Fabio de Moura Martins <fabio.mmouras@hotmail.com>
2024-07-02 10:16:51 -0300

    feat: adiciona a propriedade role à session
    Para saber na sessão o tipo de usuário que: é USER ou ADMIN

--- auth.ts
+++ auth.ts
@@ -3,6 +3,7 @@ import NextAuth from 'next-auth';
 import { PrismaAdapter } from '@auth/prisma-adapter';
 import { db } from '@/lib/db';
 import authConfig from '@/auth.config';
+import { getUserById } from '@/data/user';
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
     callbacks: {
@@ -10,9 +11,22 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
             if (token.sub && session.user) {
                 session.user.id = token.sub;
             }
+
+            if (token.role && session.user) {
+                session.user.role = token.role;
+            }
+
             return session;
         },
         async jwt({ token }) {
+            if (!token.sub) return token;
+
+            const existingUser = await getUserById(token.sub);
+
+            if (!existingUser) return token;
+
+            token.role = existingUser.role;
+
             return token;
         }
     },
